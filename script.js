bb2a8add975a157916d5338606176c4b023dddcc
Cdb2020ashe <nipuro2020@gmail.com>
2025-11-17 21:35:18 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Script para el prototipo de Carrero Manufacturas
+// Este archivo está vacío ya que el prototipo no requiere funcionalidad JavaScript
+// Todo el comportamiento es simulado visualmente con HTML y CSS
+
+console.log('Prototipo Carrero Manufacturas - Sin funcionalidad JavaScript activa');
